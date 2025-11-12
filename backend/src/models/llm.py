a806bebda456e6117d19a3ce95a014cfc6a6cad4
= <vylegzhanin.ev@mail.ru>
2025-11-13 01:29:27 +0300

    [env] pro-mode chore: add .env.example file and delete some comments

--- backend/src/models/llm.py
+++ backend/src/models/llm.py
@@ -8,14 +8,4 @@ api_key = os.getenv("API_KEY")
 
 print(f"Model: {model_name}, URL: {base_url}, key: {api_key}")
 
-llm = ChatOpenAI(
-    model=model_name,
-    verbose=True,
-    base_url=base_url,
-    api_key=api_key,
-    # timeout=120,
-    # max_tokens=2500,
-    # temperature=0.5,
-    # presence_penalty=0,
-    # top_p=0.95
-)
+llm = ChatOpenAI(model=model_name, verbose=True, base_url=base_url, api_key=api_key)
